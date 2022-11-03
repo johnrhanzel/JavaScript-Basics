aa079823c410e2d8f360b6e5e19acc9a4bd9654d
John Rhanzel <rhanzel1016@gmail.com>
2022-11-03 21:04:56 +0800

    create manipulating complex objects

---
+++ manipulateObjects.js
@@ -0,0 +1,19 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {
+      artist: "Keshi",
+      title: "Angel",
+      release_year: 2022,
+      formats: ["CD", "8T", "LP"]
+    }
+  ];
